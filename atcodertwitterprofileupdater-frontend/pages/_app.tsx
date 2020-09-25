82d078d0a2a3a90dad4b6817c5e1a96bef2eed99
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-09-25 11:49:18 +0900

    + links

--- atcodertwitterprofileupdater-frontend/pages/_app.tsx
+++ atcodertwitterprofileupdater-frontend/pages/_app.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { Provider } from "next-auth/client";
-import { Container, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -14,6 +14,19 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
             </Head>
             <Navbar bg="dark" variant="dark" sticky="top">
                 <Navbar.Brand>AtCoder Twitter Profile Updater</Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavDropdown id="nav-dropdown-links" title="Links">
+                            <NavDropdown.Item href="https://github.com/makutamoto/atcoder-twitter-profile-updater">
+                                GitHub
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="https://twitter.com/makutamoto">
+                                @makutamoto
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
             <Container className="p-4">
                 <main>
