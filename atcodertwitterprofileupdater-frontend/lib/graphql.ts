335ab895119531693ade7bd09a37814dc97b012c
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-09-25 10:23:19 +0900

    renamed environment variables.

--- atcodertwitterprofileupdater-frontend/lib/graphql.ts
+++ atcodertwitterprofileupdater-frontend/lib/graphql.ts
@@ -1,33 +1,39 @@
-import aws from 'aws-sdk';
-import AWSAppSyncClient from 'aws-appsync';
-import { DocumentNode } from 'graphql';
+import AWSAppSyncClient from 'aws-appsync'
+import { DocumentNode } from 'graphql'
 
-const AWS_REGION = process.env.AWS_REGION as string;
-if(AWS_REGION === undefined) throw "AWS_REGION is not defined.";
-const API_ENDPOINT = process.env.API_ENDPOINT as string;
-if(API_ENDPOINT === undefined) throw "API_ENDPOINT is not defined.";
+const ACCESS_KEY_ID = process.env.ACCESS_KEY_ID as string
+if (ACCESS_KEY_ID === undefined) throw 'ACCESS_KEY_ID is not defined.'
+const SECRET_ACCESS_KEY = process.env.SECRET_ACCESS_KEY
+if (SECRET_ACCESS_KEY === undefined) throw 'SECRET_ACCESS_KEY is not defined.'
+const REGION = process.env.REGION as string
+if (REGION === undefined) throw 'REGION is not defined.'
+const API_ENDPOINT = process.env.API_ENDPOINT as string
+if (API_ENDPOINT === undefined) throw 'API_ENDPOINT is not defined.'
 
 const client = new AWSAppSyncClient({
-    url: API_ENDPOINT,
-    region: AWS_REGION,
-    auth: {
-        type: "AWS_IAM",
-        credentials: () => aws.config.credentials,
+  url: API_ENDPOINT,
+  region: REGION,
+  auth: {
+    type: 'AWS_IAM',
+    credentials: {
+      accessKeyId: ACCESS_KEY_ID,
+      secretAccessKey: SECRET_ACCESS_KEY,
     },
-    disableOffline: true,
-});
+  },
+  disableOffline: true,
+})
 
 export async function mutate<V>(document: DocumentNode, variables: V) {
-    await client.mutate({
-        mutation: document,
-        variables: variables,
-    });
+  await client.mutate({
+    mutation: document,
+    variables: variables,
+  })
 }
 
 export async function query<R, V>(document: DocumentNode, variables: V) {
-    const response = await client.query<R>({
-        query: document,
-        variables: variables,
-    });
-    return response.data;
+  const response = await client.query<R>({
+    query: document,
+    variables: variables,
+  })
+  return response.data
 }
